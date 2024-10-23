0efcf3eb4a711cd8b13a23b02f864d7e703368a9
rovari <68850344+ju315@users.noreply.github.com>
2024-10-23 16:00:39 +0900

    Title: 두수의 곱과 몫, Time: 175ms, Memory: 14MB, Status: Passed - CodeTree

--- 241023/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
+++ 241023/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -4,4 +4,4 @@ const input = fs.readFileSync(0).toString().split(' ');
 const a = Number(input[0]);
 const b = Number(input[1]);
 console.log(`${a} * ${b} = ${a * b}`);
-console.log(`${a} / ${b} = ${a / b}`)
+console.log(`${a} / ${b} = ${parseInt(a / b)}`)
