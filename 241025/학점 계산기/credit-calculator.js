eb5c1f2ae508f32ef67911242fea2ad1316012ad
rovari <68850344+ju315@users.noreply.github.com>
2024-10-25 16:44:14 +0900

    Title: 학점 계산기, Time: 225ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241025/학점 계산기/credit-calculator.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const score = input[1].split(' ').map(Number);
+
+const sum = score.reduce((prev, curr) => prev += curr, 0);
+const avg = (sum/n).toFixed(1);
+
+console.log(avg)
+if (avg > 4.0) {
+    console.log('Perfect')
+} else if (avg > 3.0) {
+    console.log('Good')
+} else {
+    console.log('Poor')
+}
