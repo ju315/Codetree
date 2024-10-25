e0d7fd5bfac1284d9d51948def1575a895613e43
rovari <68850344+ju315@users.noreply.github.com>
2024-10-25 16:25:44 +0900

    Title: 특정 숫자 도달하기, Time: 200ms, Memory: 15MB, Status: Passed - CodeTree

--- 241025/특정 숫자 도달하기/reaching-specific-number.js
+++ 241025/특정 숫자 도달하기/reaching-specific-number.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const list = fs.readFileSync(0).toString().trim().split(' ').map(Number)
-const lastIdx = list.findIndex((x) => x >=250);
+const lastIdx = list.findIndex((x) => x >= 250);
+
+if (lastIdx === -1) {
+    const sum = list.reduce((prev, curr) => prev += curr, 0);
+    console.log(`${sum} ${(sum / list.length).toFixed(1)}`)
+    return;
+}
 const targetList = list.filter((_, idx) => idx < lastIdx);
 
 const sum = targetList.reduce((prev, curr) => prev += curr, 0);
