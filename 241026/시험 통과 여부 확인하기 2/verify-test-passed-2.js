575d90be8641a3bb69e0c4ea982c40a1d5b92d3a
rovari <68850344+ju315@users.noreply.github.com>
2024-10-26 21:47:56 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

--- 241026/시험 통과 여부 확인하기 2/verify-test-passed-2.js
+++ 241026/시험 통과 여부 확인하기 2/verify-test-passed-2.js
@@ -2,21 +2,18 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const student = Number(input[0]);
+let passCnt = 0;
 
 const scoreList = Array.from({length: student}, (_, idx) => {
     const score = input[idx+1].split(' ').reduce((prev, curr) => prev += Number(curr), 0) / 4
-    return score;
-});
-
-let passCnt = 0;
-
-scoreList.forEach((score) => {
     if (score >= 60) {
         console.log('pass');
         passCnt++;
     } else {
         console.log('fail')
     }
+
+    return score;
 });
 
 console.log(passCnt);
