2a72011ac1cffb7c820492f77d87513db175124e
rovari <68850344+ju315@users.noreply.github.com>
2024-10-30 11:48:27 +0900

    Title: 나눗셈의 나머지, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

--- 241030/나눗셈의 나머지/remainder-of-division.js
+++ 241030/나눗셈의 나머지/remainder-of-division.js
@@ -6,12 +6,11 @@ const b = input[1];
 
 const reminderList = new Array(b).fill(0);
 
-while(quotient >= 0) {
+while(quotient > 1) {
     const reminder = quotient%b;
     quotient = Math.floor((quotient / b));
 
     reminderList[reminder]++;
-    if (quotient === 0) break;
 }
 
 const res = reminderList.reduce((prev, curr) => prev += curr**2, 0);
