263863029c7728f6b500e7711831d24f09c45740
rovari <68850344+ju315@users.noreply.github.com>
2024-10-30 12:12:11 +0900

    Title: 배열 놀이, Time: 198ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241030/배열 놀이/play-with-array.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, queryCnt] = input[0].split(' ').map(Number);
+const list = input[1].split(' ').map(Number);
+const queryList = input.slice(2, input.length);
+
+queryList.forEach((x) => {
+    const [type, idx, idx2] = x.split(' ').map(Number);
+
+    if (type === 1) {
+        console.log(list[idx - 1]);
+    } else if (type === 2) {
+        const findIdx = list.findIndex((l) => l === idx);
+
+        console.log(findIdx > -1 ? findIdx + 1 : 0);
+    } else {
+        console.log(list[idx - 1], list[idx2 - 1])
+    }
+})
