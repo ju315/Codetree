a30551a198673ef66092a73b52025ab0de9f7944
rovari <68850344+ju315@users.noreply.github.com>
2024-10-30 11:53:04 +0900

    Title: 특정 위치의 문자, Time: 221ms, Memory: 14MB, Status: Passed - CodeTree

--- 241030/특정 위치의 문자/char-in-specific-location.js
+++ 241030/특정 위치의 문자/char-in-specific-location.js
@@ -3,6 +3,18 @@ const fs = require('fs');
 const stringList = ['L', 'E', 'B', 'R', 'O', 'S'];
 const input = fs.readFileSync(0).toString().trim();
 
-const findIdx = stringList.findIndex((x) => x === input);
+// const findIdx = stringList.findIndex((x) => x === input);
 
-console.log(findIdx > -1 ? findIdx : 'None')
+// console.log(findIdx > -1 ? findIdx : 'None');
+
+for (let i = 0; i < stringList.length; i++) {
+    const isMatch = stringList[i] === input;
+
+    if (isMatch) {
+        console.log(i)
+
+        return;
+    }
+}
+
+console.log('None')
