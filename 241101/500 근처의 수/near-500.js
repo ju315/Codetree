f738d444a6bb0d7bb15c7adc7134e235edf2e38e
rovari <68850344+ju315@users.noreply.github.com>
2024-11-01 14:02:23 +0900

    Title: 500 근처의 수, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241101/500 근처의 수/near-500.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const list = fs.readFileSync(0).toString().trim().split(' ');
+
+let maxVal = 0, minVal = 1000;
+
+for (const v of list) {
+    if (v > 500 && v < minVal) {
+        minVal = v;
+    } else if (v < 500 && v > maxVal) {
+        maxVal = v;
+    }
+}
+
+console.log(maxVal, minVal)
