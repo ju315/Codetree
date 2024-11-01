08f861791114545758ba8a9e79382f52ab0edb7b
rovari <68850344+ju315@users.noreply.github.com>
2024-11-01 13:37:35 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 208ms, Memory: 17MB, Status: Passed - CodeTree

--- 241101/중복되지 않는 숫자 중 최대/max-of-unique-number.js
+++ 241101/중복되지 않는 숫자 중 최대/max-of-unique-number.js
@@ -2,6 +2,18 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-const maxNum = [...new Set(input[1].split(' ').map(Number))].sort((a, b) => b - a)[0];
+const list = input[1].split(' ').map(Number).sort((a, b) => b - a);
 
-console.log(maxNum || -1)
+for (let i = 0; i < n; i++) {
+    const v = list[i];
+
+    const target = list.filter((x) => x === v);
+
+    if (target.length === 1) {
+        console.log(v);
+
+        return;
+    }
+}
+
+console.log(-1)
