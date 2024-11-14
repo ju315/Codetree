6d6a551a1d17a6a758e058e3fe41a5a8e4856496
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-11-14 14:46:09 +0900

    Title: 문자열 범위 출력하기 2, Time: 195ms, Memory: 15MB, Status: Passed - CodeTree

--- 241114/문자열 범위 출력하기 2/print-string-in-range-2.js
+++ 241114/문자열 범위 출력하기 2/print-string-in-range-2.js
@@ -3,7 +3,7 @@ const [str, n] = fs.readFileSync(0).toString().trim().split('\n');
 const revStr = str.split('').reverse().join('');
 
 if (n >= str.length) {
-    consolel.og(revStr)
+    console.log(revStr)
 
     return;
 }
