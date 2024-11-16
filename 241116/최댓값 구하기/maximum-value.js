9ad3104da379fde654f3a09aa7313caf854ab701
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-11-16 13:38:33 +0900

    Title: 최댓값 구하기, Time: 166ms, Memory: 15MB, Status: Passed - CodeTree

--- 241116/최댓값 구하기/maximum-value.js
+++ 241116/최댓값 구하기/maximum-value.js
@@ -1,4 +1,15 @@
 const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+const [a, b, c] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
-console.log(Math.max(...input))
+let ans = 0;
+if (a > b) {
+    ans = a;
+} else {
+    ans = b;
+}
+
+if (ans < c) {
+    console.log(c)
+} else {
+    console.log(ans)
+}
