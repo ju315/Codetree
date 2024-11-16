0244b99a5cc701995ffa0c0d944b3b7cf65402b3
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-11-16 13:34:17 +0900

    Title: 코로나 메뉴얼, Time: 185ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241116/코로나 메뉴얼/covid-manual.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const patient = fs.readFileSync(0).toString().trim().split('\n')
+
+const cnt = patient.reduce((prev, curr) => {
+    const [state, degree] = curr.split(' ');
+
+    if (state === 'Y' && degree >= '38') {
+        prev++;
+    }
+
+    return prev;
+}, 0)
+
+
+console.log(cnt >=2 ? 'E' : 'N')
