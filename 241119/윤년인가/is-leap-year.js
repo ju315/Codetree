5f14b768f490e54942290c9ee548683b79ee3517
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-11-19 08:35:57 +0900

    Title: 윤년인가, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

--- 241119/윤년인가/is-leap-year.js
+++ 241119/윤년인가/is-leap-year.js
@@ -2,7 +2,11 @@ const fs = require('fs');
 const y = Number(fs.readFileSync(0).toString().trim());
 
 if (y%4 === 0) {
-    console.log(true)
+    if (y%100 === 0 && y%400 !== 0) {
+        console.log(false)
+    } else {
+        console.log(true)
+    }
 } else {
-    console.log(y%100 === 0 && y%400 === 0)
+    console.log(false);
 }
