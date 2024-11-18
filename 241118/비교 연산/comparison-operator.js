0df6a41cfc6f4e54dded4d9b2948fcb91196a2e3
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-11-18 12:40:11 +0900

    Title: 비교 연산, Time: 176ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241118/비교 연산/comparison-operator.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+console.log(Number(a >= b));
+console.log(Number(a > b));
+console.log(Number(a <= b));
+console.log(Number(a < b));
+console.log(Number(a === b));
+console.log(Number(a !== b));
