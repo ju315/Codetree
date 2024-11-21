2e843872f0d07a70daf5b367950e3a7795d4d28f
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-11-21 21:41:29 +0900

    Title: 특정 숫자 맞추기, Time: 183ms, Memory: 15MB, Status: Passed - CodeTree

--- 241121/특정 숫자 맞추기/catching-specific-number.js
+++ 241121/특정 숫자 맞추기/catching-specific-number.js
@@ -3,5 +3,8 @@ const list = require('fs').readFileSync(0).toString().trim().split('\n').map(Num
 for (const data of list) {
     if (data < 25) console.log('Higher');
     if (data > 25) console.log('Lower');
-    if (data === 25) console.log('Good')
+    if (data === 25) {
+        console.log('Good')
+        break;
+    }
 }
