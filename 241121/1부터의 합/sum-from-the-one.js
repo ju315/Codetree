7ca1b5fc277e8f96ef28949fb758bd20064db374
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-11-21 21:28:01 +0900

    Title: 1부터의 합, Time: 183ms, Memory: 15MB, Status: Passed - CodeTree

--- 241121/1부터의 합/sum-from-the-one.js
+++ 241121/1부터의 합/sum-from-the-one.js
@@ -1,10 +1,13 @@
 const n = Number(require('fs').readFileSync(0).toString().trim());
 
-let res = 1;
+let res = 0;
+let acc = 0;
 
-for (let i = 2; ; i++) {
-    if (res + i >= n) break;
-    res += i;
+for (let i = 1; i <= 100; i++) {
+    if (acc + i >= n) {
+        console.log(i)
+        break;
+    }
+
+    acc += i;
 }
-
-console.log(res)
