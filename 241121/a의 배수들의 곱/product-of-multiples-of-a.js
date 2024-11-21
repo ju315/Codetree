d4bee7c4fd5b40a7d9c94ffe0e0c6fdfb9d410a7
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-11-21 21:02:54 +0900

    Title: a의 배수들의 곱, Time: 174ms, Memory: 15MB, Status: Passed - CodeTree

--- 241121/a의 배수들의 곱/product-of-multiples-of-a.js
+++ 241121/a의 배수들의 곱/product-of-multiples-of-a.js
@@ -3,7 +3,7 @@ const [a, b] = require('fs').readFileSync(0).toString().trim().split(" ").map(Nu
 let res = 1;
 
 for (let i= 1; i <=b; i++) {
-    if (a%i === 0) res *= i;
+    if (i%a === 0) res *= i;
 }
 
 console.log(res)
