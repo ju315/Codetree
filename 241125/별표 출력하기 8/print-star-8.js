5076ad222556b5323ed77888e2a61655702bfb73
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-11-25 21:08:31 +0900

    Title: 별표 출력하기 8, Time: 160ms, Memory: 15MB, Status: Passed - CodeTree

--- 241125/별표 출력하기 8/print-star-8.js
+++ 241125/별표 출력하기 8/print-star-8.js
@@ -1,10 +1,24 @@
 const n = Number(require('fs').readFileSync(0).toString().trim());
 
+// for (let i = 1; i <= n; i++) {
+//     if (i%2 === 1) {
+//         console.log('*')
+//         continue;
+//     }
+
+//     console.log('* '.repeat(i))
+// }
+
 for (let i = 1; i <= n; i++) {
     if (i%2 === 1) {
         console.log('*')
         continue;
     }
 
-    console.log('* '.repeat(i))
+    let str = '';
+    for (let j = 1; j <= i; j++) {
+        str += '* '
+    }
+
+    console.log(str)
 }
