29fad11736ca293589aca6f28507918b213da720
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-11-27 20:22:39 +0900

    Title: 1-9 정사각형, Time: 184ms, Memory: 15MB, Status: Passed - CodeTree

--- 241127/1-9 정사각형/1-9-square.js
+++ 241127/1-9 정사각형/1-9-square.js
@@ -5,7 +5,9 @@ for (let i = 0; i < n; i++) {
     let str = '';
     for (let j = 0; j < n; j++) {
         str += num;
-        num++;
+        
+        if (num === 9) num = 1
+        else num++;
     }
 
     console.log(str)
