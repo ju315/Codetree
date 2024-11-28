571f668e89801719245bea452d2dd894e20dd1fa
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-11-28 23:31:14 +0900

    Title: 숫자 피라미드, Time: 167ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241128/숫자 피라미드/number-pyramid.js
@@ -0,0 +1,9 @@
+const n = Number(require('fs').readFileSync(0).toString().trim());
+
+for (let i = 1; i <= n; i++) {
+    let str = '';
+    for (let j = 1; j <= i; j++) {
+        str += i + ' ';
+    }
+    console.log(str)
+}
