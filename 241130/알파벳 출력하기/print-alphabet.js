dd01670bb2c18dee5b7f7d07076debe5eae6c41e
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-11-30 17:49:42 +0900

    Title: 알파벳 출력하기, Time: 176ms, Memory: 15MB, Status: Passed - CodeTree

--- 241130/알파벳 출력하기/print-alphabet.js
+++ 241130/알파벳 출력하기/print-alphabet.js
@@ -1,11 +1,11 @@
 const n = Number(require('fs').readFileSync(0).toString().trim());
-
 let charCode = 65;
 for (let i = 1; i <=n; i++) {
     let str = ''
     for (let j = 1; j <= i; j++) {
         str += String.fromCharCode(charCode);
-        charCode++;
+        if (charCode === 90) charCode = 65;
+        else charCode++;
     }
 
     console.log(str)
