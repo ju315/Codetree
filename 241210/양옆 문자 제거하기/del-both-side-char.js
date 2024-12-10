926b44a125c42dd2f5bda27eb2fd4b64cd95c242
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-12-10 23:46:45 +0900

    Title: 양옆 문자 제거하기, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

--- 241210/양옆 문자 제거하기/del-both-side-char.js
+++ 241210/양옆 문자 제거하기/del-both-side-char.js
@@ -1,3 +1,3 @@
 const text = require('fs').readFileSync(0).toString().trim();
 
-console.log(text.slice(0, 2) + text.slice(3, text.length - 2) + text.slice(text.length - 1))
+console.log(text.slice(0, 1) + text.slice(2, text.length - 2) + text.slice(text.length - 1))
