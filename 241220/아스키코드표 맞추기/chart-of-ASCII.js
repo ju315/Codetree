6c1255f0ed1a6f93630bb06218b7063c59a9e139
ByeongJu Park <68850344+ju315@users.noreply.github.com>
2024-12-20 15:32:45 +0900

    Title: 아스키코드표 맞추기, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

--- 241220/아스키코드표 맞추기/chart-of-ASCII.js
+++ 241220/아스키코드표 맞추기/chart-of-ASCII.js
@@ -1,3 +1,3 @@
-const res = require('fs').readFileSync(0).toString().split(' ').map((code) => String.fromCharCode(code)).join(' ');
+const res = require('fs').readFileSync(0).toString().trim().split(' ').map((code) => String.fromCharCode(code)).join(' ');
 
 console.log(res)
